7e42b33dc9f3fe4cb31014b1e1fbb21a3f8b2137
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-31 12:44:47 +0100

    Útvonalak regisztrálása

--- Kreta.Maui/AppShell.xaml.cs
+++ Kreta.Maui/AppShell.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace Kreta.Maui
+﻿using Kreta.Maui.Pages;
+
+namespace Kreta.Maui
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(LoadingPage),typeof(LoadingPage));
+            Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            Routing.RegisterRoute(nameof(ProfilPage), typeof(ProfilPage));
         }
     }
 }
