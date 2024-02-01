e376945952fab4cae42cc491673cf46ebd0abd58
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-01 09:16:22 +0100

    CommunityToolkit.Mvvm és LoginViewModel, abban Login parancs

---
+++ Kreta.Maui/ViewModel/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Kreta.Maui.ViewModel
+{
+    public partial class LoginViewModel : ObservableObject
+    {
+        [RelayCommand]
+        public void Login()
+        { }
+    }
+}
