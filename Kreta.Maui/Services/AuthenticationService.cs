176120488b8e036de60192f161a81f130d2947ed
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-01 09:50:47 +0100

    Autentikációs állapot tárolása

--- Kreta.Maui/Services/AuthenticationService.cs
+++ Kreta.Maui/Services/AuthenticationService.cs
@@ -2,10 +2,21 @@
 {
     public class AuthenticationService
     {
+        private const string AuthenticationStateKey = "AuthenticationState";
         public async Task<bool> IsAuthenticatedAsync()
         {
             await Task.Delay(200);
             return false;
+        }
+
+        public void Login()
+        {
+            Preferences.Default.Set<bool>(AuthenticationStateKey, true);
+        }
+
+        public void Logout()
+        {
+            Preferences.Default.Remove(AuthenticationStateKey);
         }
             
     }
